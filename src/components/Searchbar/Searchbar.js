c360ccbf67b4415bf2a9061165da20c84f5370a8
= <a.vaulin@loanscan.io>
2019-07-22 20:04:41 +0200

    added searchbar

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { withRouter } from 'react-router-dom';
+
+const Searchbar = ({ history }) => {
+  const [value, setValue] = useState('');
+  const searchClick = () => {
+    value && history.push(`/account/${value}`);
+  };
+  return (
+    <SearchContainer>
+      <SearchWrapper>
+        <SearchInput
+          type="text"
+          value={value || ''}
+          onChange={e => setValue(e.target.value)}
+          placeholder="Explore by name or account hash"
+        />
+        <SearchButton className="search-button" type="button" value="Explore" onClick={searchClick} />
+      </SearchWrapper>
+    </SearchContainer>
+  );
+};
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 0 20px 0;
+`;
+const SearchWrapper = styled.div`
+  display: flex;
+  flex-grow: 1;
+  flex-direction: row;
+  padding: 4px;
+  border-radius: 3px;
+  color: rgba(255, 255, 255, 0.42);
+  background-color: #30313b;
+  align-content: flex-end;
+`;
+const SearchInput = styled.input`
+  padding: 8px 16px;
+  flex-grow: 1;
+  font-size: 16px;
+  font-weight: 100;
+  color: rgba(255, 255, 255, 0.52);
+  background: transparent;
+  border: none;
+  outline: none;
+`;
+const SearchButton = styled.input`
+  border: 0;
+  padding: 4px 32px;
+  background-color: #4ab4cb;
+  letter-spacing: 0.04em;
+  border-radius: 3px;
+  text-transform: uppercase;
+  color: #fff;
+  line-height: 24px;
+  cursor: pointer;
+`;
+
+export default withRouter(Searchbar);
