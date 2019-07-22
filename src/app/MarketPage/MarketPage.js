db8b65c6a2b250f888111980ec21ad1dd286628d
= <a.vaulin@loanscan.io>
2019-07-22 20:08:41 +0200

    added market page

---
+++ src/app/MarketPage/MarketPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import PriceHistory from '../../components/PriceHistory';
+import VolumeHistory from '../../components/VolumeHistory';
+
+const Home = props => {
+  return (
+    <Wrapper>
+      <VolumeHistory />
+      <PriceHistory />
+    </Wrapper>
+  );
+};
+const Wrapper = styled.div``;
+const TwoElementsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  flex-basis: 500px;
+  justify-content: space-between;
+  margin: 0 -5px;
+`;
+export default Home;
