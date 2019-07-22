533d327177088dcc75b62854bed2f311e2013e96
= <a.vaulin@loanscan.io>
2019-07-22 19:59:30 +0200

    refactoring

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import HomePage from './HomePage/HomePage';
+import AccountPage from './AccountPage/AccountPage';
+import MarketPage from './MarketPage/MarketPage';
 
 import '../styles/css/index.css';
 
@@ -12,6 +14,8 @@ const App = () => {
         <Switch>
           <Redirect exact from="/" to="/home" />
           <Route path="/home" component={HomePage} />
+          <Route path="/account/:hash" component={AccountPage} />
+          <Route path="/market" component={MarketPage} />
         </Switch>
       </Layout>
     </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import setDefaultGlobalState from './services/globalState';
 
+setDefaultGlobalState();
 const render = () => {
   return ReactDOM.render(<App />, document.getElementById('root'));
 };
 
 render();
-
-serviceWorker.unregister();
