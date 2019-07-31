b70ef3588c1d198617953841051cb13107593be4
= <a.vaulin@loanscan.io>
2019-07-31 10:30:24 +0200

    added new utils

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,9 @@
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
 import { format } from 'd3-format';
+import { backerAccounts } from './config/backer-accounts';
 import _ from 'lodash';
+import StakingBond from './components/StakingBond';
 
 TimeAgo.addLocale(en);
 export const timeAgo = new TimeAgo('en-US');
@@ -40,15 +42,11 @@ export function formatCurrency(value, prefix = ',', symbol = 'ꜩ') {
 export const addCommas = format(',');
 
 export function wrapTxs(res) {
-  let txVolume = [{
-    id: 'tx-volume',
-    color: '#1af3f9',
-    data: []
-  }]
-  res.map(item => {
-    txVolume[0].data.push({ x: item[0], y: item[4] });
+
+  return res.map(item => {
+    return { time: new Date(item[0]), value: item[4] };
   });
-  return txVolume;
+
 }
 
 export function wrapFlowData(flowData, account) {
@@ -56,19 +54,20 @@ export function wrapFlowData(flowData, account) {
   let outFlowData = { id: 'Out-flow', color: '#83899B', data: [], };
 
   let spandableBalance = account.spendable_balance;
-
+  let dataInOut = [];
   //[0]-time [1]-in [2]-out
   flowData.map((item, i) => {
 
     let curentBalanceIn = flowData[i] ? flowData[i][1] : 0;
     let curentBalanceOut = flowData[i] ? flowData[i][2] : 0;
 
-    let inflow = (spandableBalance - curentBalanceIn)
+    let inFlow = (spandableBalance - curentBalanceIn)
     let outFlow = (spandableBalance + curentBalanceOut)
-    inFlowData.data.push({ x: item[0], y: inflow });
+    inFlowData.data.push({ x: item[0], y: inFlow });
     outFlowData.data.push({ x: item[0], y: -outFlow });
+    dataInOut.push({ time: item[0], inFlow: inFlow, outFlow: -outFlow })
   });
-  return { inFlowData, outFlowData };
+  return { inFlowData, outFlowData, dataInOut };
 }
 
 export function wrapToVolume(marketData) {
@@ -97,7 +96,7 @@ export function wrapStakingData({ balance, deposits, rewards, fees, account }) {
   let frozenDeposit = account.frozen_deposits;
   let frozenRewards = account.frozen_rewards;
   let frozenFees = account.frozen_fees;
-
+  let allData = [];
   //[0]-time [1]-in [2]-out
   balance.map((item, i) => {
 
@@ -125,31 +124,16 @@ export function wrapStakingData({ balance, deposits, rewards, fees, account }) {
     currentDeposit.data.push({ x: balance[i][0], y: (frozenDeposit) });
     //frozen rewards + frozen fees
     pendingReawards.data.push({ x: balance[i][0], y: frozenRewards + frozenFees });
+    allData.push({
+      time: item[0],
+      bond: spandableBalance + frozenDeposit,
+      deposit: frozenDeposit,
+      rewards: frozenRewards + frozenFees
+    })
   });
-  return { stackingBond, currentDeposit, pendingReawards };
-}
-
-export function wrapBlockData(blockData) {
-  let stackingBond = {
-    id: 'Stacking Bond',
-    color: '#1af3f9',
-    data: []
-  };
-  let currentDeposit = {
-    id: 'Current Deposit',
-    color: '#83899B',
-    data: [],
-  };
-  let pendingReawards = {
-    id: 'Pending Rewards',
-    color: '#83899B',
-    data: [],
-  };
-  blockData.map((item) => {
-    stackingBond.data.push({ x: item[0], y: item[1].toFixed() });
-    currentDeposit.data.push({ x: item[0], y: item[2].toFixed() });
-    pendingReawards.data.push({ x: item[0], y: item[2].toFixed() });
-  });
+  stackingBond.data = stackingBond.data.reverse()
+  currentDeposit.data = currentDeposit.data.reverse()
+  pendingReawards.data = pendingReawards.data.reverse()
   return { stackingBond, currentDeposit, pendingReawards };
 }
 
@@ -169,5 +153,59 @@ export function getShortHash(hash) {
 
 export function capitalizeFirstLetter(str) {
   return `${str[0].toUpperCase() + str.slice(1)}`;
+}
+
+export function get60mTimeRange(lastTime) {
+  let timeArray60m = [];
+  const length = lastTime - 60000 * 60;
+  for (let index = lastTime; index > length; index = index - 60000) {
+    timeArray60m.push(index);
+  }
+  return timeArray60m;
+}
+
+export function wrappBlockDataToObj(array) {
+  return array.reduce((obj, item) => {
+    obj[new Date(item[0]).setSeconds(0, 0)] = {
+      time: new Date(item[0]).setSeconds(0, 0),
+      hash: item[1],
+      height: item[2],
+      priority: item[3],
+      opacity: item[3] === 0
+        ? 1
+        : item[3] < 8
+          ? 0.8
+          : item[3] < 16
+            ? 0.6
+            : item[3] < 32
+              ? 0.4
+              : 0.2
+    }
+    return obj
+  }, {})
+}
+
+export function getDelegatorByHash(hash) {
+  return Object.keys(backerAccounts).filter(r => backerAccounts[r] === hash);
+}
+
+export function getPeakVolumeTime(data) {
+  const time_0_4 = { total: _.sumBy(data.slice(0, 30), (o) => o.value), title: "00:00 - 04:00" };
+  const time_4_8 = { total: _.sumBy(data.slice(30, 60), (o) => o.value), title: "04:00 - 08:00" };
+  const time_8_12 = { total: _.sumBy(data.slice(60, 90), (o) => o.value), title: "08:00 - 12:00" };
+  const time_12_16 = { total: _.sumBy(data.slice(90, 120), (o) => o.value), title: "12:00 - 16:00" };
+  const time_16_20 = { total: _.sumBy(data.slice(120, 150), (o) => o.value), title: "16:00 - 20:00" };
+  const time_20_24 = { total: _.sumBy(data.slice(150, 180), (o) => o.value), title: "20:00 - 24:00" };
+
+  const peak = _.maxBy([time_0_4, time_4_8, time_8_12, time_12_16, time_16_20, time_20_24], (o) => o.total).title
+  return peak;
+}
+
+export function getDailyVolume(data) {
+  return _.sumBy(data, (o) => o.volume) / data.length;
+}
+
+export function convertToTitle(str) {
+  return str.split("_").map(r => capitalizeFirstLetter(r)).join(' ');
 }
 
