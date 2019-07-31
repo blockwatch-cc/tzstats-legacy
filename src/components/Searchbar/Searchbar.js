b4deeedfed14ce11eb9cce67017a2d06f85d6528
= <a.vaulin@loanscan.io>
2019-07-31 10:38:32 +0200

    added search bar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -2,37 +2,110 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
 import { backerAccounts } from '../../config/backer-accounts';
+import { FlexRow, DataBox } from '../Common';
+import useKeyPress from "../../hooks/useKeyPress"
+import useLocalStorage from "../../hooks/useLocalStorage"
+
 
 const Searchbar = ({ history }) => {
   const [value, setValue] = useState('');
-  const searchClick = () => {
+  const [isFocus, setIsFocus] = useState(false);
+  const [enterPress, setKeyPressed] = useKeyPress(13);
+  const [suggestions, setSuggestions] = useLocalStorage('suggestions');
 
-    if (value.length === 36)
-      value && history.push(`/account/${value}`);
-    else if (value.length === 51)
-      value && history.push(`/block/${value}`);
-    else
-      value && history.push(`/block/${backerAccounts[0]}`);
+  const search = (searchValue) => {
+    setKeyPressed(false)
+
+    if (searchValue.length === 36) {
+      setSuggestions([{ type: "Account", value: searchValue }, ...suggestions])
+      searchValue && history.push(`/account/${searchValue}`);
+    }
+    else if (searchValue[0] === 'B' || parseInt(searchValue)) {
+      setSuggestions([{ type: "Block", value: searchValue }, ...suggestions])
+      searchValue && history.replace(`/block/${searchValue}`)
+    }
+    else if (searchValue[0] === 'o') {
+      setSuggestions([{ type: "Operation", value: searchValue }, ...suggestions])
+      searchValue && history.push(`/operation/${searchValue}`);
+    }
   };
+
   return (
     <SearchContainer>
       <SearchWrapper>
+        {enterPress && search(value)}
         <SearchInput
           type="text"
           value={value || ''}
           onChange={e => setValue(e.target.value)}
-          placeholder="Explore by name or account hash"
+          onMouseEnter={e => setIsFocus(true)}
+          onFocus={e => setIsFocus(true)}
+          onBlur={e => setIsFocus(false)}
+          placeholder="Explore blocks, operations, accounts, elections, and cycles …"
         />
-        <SearchButton className="search-button" type="button" value="Explore" onClick={searchClick} />
       </SearchWrapper>
+      {isFocus && suggestions.length &&
+        <AutocompleteWrapper onMouseLeave={e => setIsFocus(false)}>
+          <Title>Recent History</Title>
+          {
+            suggestions.map((item) => {
+
+              return (
+                <AutocompleteItem onClick={e => search(item.value)}>
+                  <TypeSearch>
+                    {item.type}
+                  </TypeSearch>
+                  {item.value}
+                </AutocompleteItem>
+              )
+            })
+          }
+        </AutocompleteWrapper>
+      }
     </SearchContainer>
   );
 };
+
+const Title = styled.div`
+    color: rgba(255, 255, 255, 0.52);
+    font-size: 10px;
+    margin: 10px;
+    `;
+
+const AutocompleteItem = styled(FlexRow)`
+  padding:10px;
+  &:hover {
+      opacity: 0.8;
+      cursor:pointer;
+      background: #424552;
+    }
+  `
+
+const TypeSearch = styled.div`
+      width: 150px;
+  `;
+const AutocompleteWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  position:absolute;
+  top:62px;
+  height:220px;
+  z-index:1000;
+  overflow: scroll;
+  opacity:0.95;
+  width:inherit;
+  border-radius: 3px;
+  margin-top: 1px;
+  font-size: 12px;
+  background-color: #262830;
+  
+  `;
 const SearchContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   padding: 10px 0 20px 0;
-`;
+  `;
 const SearchWrapper = styled.div`
   display: flex;
   flex-grow: 1;
@@ -42,7 +115,7 @@ const SearchWrapper = styled.div`
   color: rgba(255, 255, 255, 0.42);
   background-color: #30313b;
   align-content: flex-end;
-`;
+  `;
 const SearchInput = styled.input`
   padding: 8px 16px;
   flex-grow: 1;
@@ -52,17 +125,6 @@ const SearchInput = styled.input`
   background: transparent;
   border: none;
   outline: none;
-`;
-const SearchButton = styled.input`
-  border: 0;
-  padding: 4px 32px;
-  background-color: #4ab4cb;
-  letter-spacing: 0.04em;
-  border-radius: 3px;
-  text-transform: uppercase;
-  color: #fff;
-  line-height: 24px;
-  cursor: pointer;
-`;
+  `;
 
 export default withRouter(Searchbar);
