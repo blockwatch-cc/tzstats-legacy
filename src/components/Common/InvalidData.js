dca27191fe87723a13bc479f828adc67b9e9b371
= <a.vaulin@loanscan.io>
2019-08-14 19:37:55 +0200

    fix smile png

--- src/components/Common/InvalidData.js
+++ src/components/Common/InvalidData.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import Card from './Card';
-import smile from '../../assets/img/smile.png';
 
 const IvalidData = ({ title = 'Something went wrong' }) => {
   return (
