95f9b35c7091b676119f702dfbf414a7e9571a1d
Alexander Eichhorn <alex@kidtsunami.com>
2019-09-05 13:34:31 +0200

    add StakeNow

--- src/app/BlockPage/BlockPage.js
+++ src/app/BlockPage/BlockPage.js
@@ -15,10 +15,8 @@ const BlockPage = ({ match, history }) => {
   React.useEffect(() => {
     const fetchData = async () => {
       let [block, lastBlock] = await Promise.all([getBlock(currentBlockHash), getBlock()]);
-      //todo optimize it for blockNumber
       const onehour = 60*60*1000;
       const historySpan = (new Date(lastBlock.time) - new Date(block.time) + block.solvetime*1000)
-      console.log("BLOCK time diff",historySpan, lastBlock.time, block.time, block.solvetime );
       let blockHistory = [];
       if (historySpan < onehour) {
         blockHistory = await getBlockRange(lastBlock.height, 60, 0);
--- src/config/baker-accounts.js
+++ src/config/baker-accounts.js
@@ -185,5 +185,6 @@ export const bakerAccounts = {
   "tz1KfEsrtDaA1sX7vdM4qmEPWuSytuqCDp5j": { name: "XTZMaster", logo: null, link: "", fee_type: "", fee: 0 },
   "tz1eZwq8b5cvE2bPKokatLkVMzkxz24z3Don": { name: "You loaf We bake", logo: null, link: "", fee_type: "", fee: 0 },
   "tz1L3vFD8mFzBaS8yLHFsd7qDJY1t276Dh8i": { name: "Zed Node", logo: null, link: "", fee_type: "", fee: 0 },
-  "tz1KtvGSYU5hdKD288a1koTBURWYuADJGrLE": { name: "ØCrypto Pool", logo: null, link: "", fee_type: "", fee: 0 }
+  "tz1KtvGSYU5hdKD288a1koTBURWYuADJGrLE": { name: "ØCrypto Pool", logo: null, link: "", fee_type: "", fee: 0 },
+  "tz1g8vkmcde6sWKaG2NN9WKzCkDM6Rziq194": { name: "StakeNow", logo: null, link: "", fee_type: "", fee: 0 }
 };
