8cb3291704175a91e19dec95a8af84c208889561
Alexander Eichhorn <alex@blockwatch.cc>
2020-02-17 12:31:38 +0100

    add activation height to proposals

--- src/config/proposals.js
+++ src/config/proposals.js
@@ -5,7 +5,8 @@ export const proposals = {
     by: '',
     link: '',
     docu: '',
-    archive: ''
+    archive: '',
+    height: -1
   },
   Pt24m4xiPbLDhVgVfABUjirbmda3yohdN82Sp9FeuAXJ4eV9otd: {
     id: 11,
@@ -14,6 +15,7 @@ export const proposals = {
     link: 'https://blog.nomadic-labs.com/athens-proposals-injected.html',
     docu: 'http://tezos.gitlab.io/master/protocols/004_Pt24m4xi.html',
     archive: 'https://blog.nomadic-labs.com/files/Athens_proposal_A.tar',
+    height: 458753
   },
   Psd1ynUBhMZAeajwcZJAeq5NrxorM6UCU4GJqxZ7Bx2e9vUWB6z: {
     name: 'Athens B',
@@ -22,6 +24,7 @@ export const proposals = {
     link: 'https://blog.nomadic-labs.com/athens-proposals-injected.html',
     docu: 'http://tezos.gitlab.io/master/protocols/004_Pt24m4xi.html',
     archive: 'https://blog.nomadic-labs.com/files/Athens_proposal_B.tar',
+    height: -1
   },
   PtdRxBHvc91c2ea2evV6wkoqnzW7TadTg9aqS9jAn2GbcPGtumD: {
     id: 12,
@@ -29,6 +32,7 @@ export const proposals = {
     by: 'OCamlPro',
     link: 'https://www.reddit.com/r/tezos/comments/by5xy8/proposal_for_amendment_brest_a/',
     archive: 'https://gitlab.com/tzscan/brest-amendment',
+    height: -1
   },
   PsBABY5nk4JhdEv1N1pZbt6m6ccB9BfNqa23iKZcHBh23jmRS9f: {
     id: 13,
@@ -37,6 +41,7 @@ export const proposals = {
     link: 'https://blog.nomadic-labs.com/babylon-proposal-injected.html',
     docu: 'tezos.gitlab.io/master/protocols/005_babylon.html',
     archive: 'https://blog.nomadic-labs.com/files/babylon_005_PsBABY5n.tar',
+    height: -1
   },
   PsBABY5HQTSkA4297zNHfsZNKtxULfL18y95qb3m53QJiXGmrbU: {
     id: 13,
@@ -45,6 +50,7 @@ export const proposals = {
     link: 'https://blog.nomadic-labs.com/babylon-proposal-injected.html',
     docu: 'tezos.gitlab.io/master/protocols/005_babylon.html',
     archive: 'https://blog.nomadic-labs.com/files/babylon_005_PsBABY5H.tar',
+    height: -1
   },
   PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS: {
     id: 13,
@@ -53,6 +59,7 @@ export const proposals = {
     link: 'https://blog.nomadic-labs.com/mainnet-release-to-patch-babylon.html',
     docu: 'http://tezos.gitlab.io/master/protocols/005_babylon.html#bug-affecting-bigmaps-in-005-psbaby5h',
     archive: 'https://blog.nomadic-labs.com/files/babylon_005_PsBabyM1.tar',
+    height: 655361
   },
   PtCarthavAMoXqbjBPVgDCRd5LgT7qqKWUPXnYii3xCaHRBMfHH: {
     id: 15,
@@ -61,6 +68,7 @@ export const proposals = {
     link: 'https://blog.nomadic-labs.com/carthage-changelog-and-testnet.html',
     docu: 'http://tezos.gitlab.io/protocols/006_carthage.html',
     archive: 'https://blog.nomadic-labs.com/files/carthage_006_PtCartha.tar',
+    height: -1
   },
   PsCARTHAGazKbHtnKfLzQg3kms52kSRpgnDY982a9oYsSXRLQEb: {
     id: 16,
@@ -69,6 +77,7 @@ export const proposals = {
     link: 'https://medium.com/cryptium/on-the-carthage-proposal-and-the-carthagenet-test-network-de876930445e',
     docu: 'https://tezos.gitlab.io/protocols/006_carthage.html',
     archive: 'https://gitlab.com/nomadic-labs/tezos/tree/proto-006',
+    height: 851969
   },
 };
 
@@ -93,7 +102,8 @@ export function getProposalById(value) {
     name: hashes[0] ? hashes[0].slice(0, 7) + '...' : '-',
     link: 'https://www.tezosagora.org/',
     docu: '',
-    archive:''
+    archive:'',
+    height: -1
   };
 }
 
@@ -110,6 +120,23 @@ export function getProposalByHash(value) {
     name: value.slice(0, 7) + '...',
     link: 'https://www.tezosagora.org/',
     docu: '',
-    archive:''
+    archive:'',
+    height: -1
+  };
+}
+
+export function getProposalByHeight(value) {
+  if (!value) {
+      return proposals.empty;
+  }
+  const hashes = Object.keys(proposals).filter(k => proposals[k].height === value);
+  return hashes[0] ? proposals[hashes[0]] : {
+    id: value,
+    by: '',
+    name: hashes[0] ? hashes[0].slice(0, 7) + '...' : '-',
+    link: 'https://www.tezosagora.org/',
+    docu: '',
+    archive:'',
+    height: -1
   };
 }
