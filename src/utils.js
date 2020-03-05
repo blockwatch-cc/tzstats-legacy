a1ead9f1114f6f2c7d99987a6c155749abb50a4e
Alexander Eichhorn <alex@blockwatch.cc>
2020-03-05 16:58:40 +0100

    fix v002 magic delegate issue

--- src/utils.js
+++ src/utils.js
@@ -320,7 +320,7 @@ export function getAccountType(a) {
   if (a.is_delegated || (a.address.startsWith('KT') && !a.is_contract)) {
     return { name: 'Delegator Account', type: 'delegator' };
   }
-  if (a.address.startsWith('tz') && a.is_delegate) {
+  if (a.address.startsWith('tz') && a.is_delegate && !a.is_delegated) {
     return { name: 'Baker Account', type: 'baker' };
   }
   if (a.is_contract) {
